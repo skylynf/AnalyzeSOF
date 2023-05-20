0226c7d41b98c32927142ac24f35bb2da2c0da63
yuki <12012640@mail.sustech.edu.cn>
2023-05-20 14:56:04 +0800

    feat(cors, api): Cors configuration for frontend and 4 apis

---
+++ src/main/java/cse/java2/project/CorsConfig.java
@@ -0,0 +1,18 @@
+package cse.java2.project;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("http://localhost:9092") // 允许访问的前端应用的地址
+        .allowedMethods("GET") // 允许的请求方法
+        .allowCredentials(true) // 允许发送身份凭证（如Cookie）
+        .maxAge(3600); // 预检请求的有效期
+  }
+}
--- src/main/java/cse/java2/project/controller/DemoController.java
+++ src/main/java/cse/java2/project/controller/DemoController.java
@@ -2,9 +2,26 @@ package cse.java2.project.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Controller
 public class DemoController {
+    // 从本地文件系统读取 JSON 文件(路径在src/main/resources下)
+    ClassPathResource resource = new ClassPathResource("result.json");
+    byte[] fileBytes = Files.readAllBytes(Path.of(resource.getURI()));
+    // 使用Jackson库解析JSON数据
+    ObjectMapper objectMapper = new ObjectMapper();
+    JsonNode jsonNode = objectMapper.readTree(fileBytes);
+    public DemoController() throws IOException {
+    }
 
     /**
      * This method is called when the user requests the root URL ("/") or "/demo".
@@ -16,5 +33,45 @@ public class DemoController {
     public String demo() {
         return "demo";
     }
+
+
+    /**
+     * This method is called when the user requests the JSON file ("/result").
+     * It reads the JSON file from the local filesystem and sends it as a response.
+     * @return the JSON file as a response entity
+     * @throws IOException if there is an error reading the file
+     */
+    @GetMapping("/result")
+    // 前端爆栈
+    public ResponseEntity<byte[]> getFile() throws IOException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setContentDispositionFormData("attachment", "result.json");
+        return ResponseEntity.ok().headers(headers).body(fileBytes);
+    }
+
+    @GetMapping("/PercentageWithoutAnswers")
+    public ResponseEntity<Double> getPercentageWithoutAnswers() throws IOException {
+        double percentageWithoutAnswers = jsonNode.get("percentage_without_answers").asDouble();
+        return ResponseEntity.ok(percentageWithoutAnswers);
+    }
+    @GetMapping("/AverageAnswers")
+    public ResponseEntity<Double> getAverageAnswers() throws IOException {
+        double averageAnswers = jsonNode.get("average_answers").asDouble();
+        return ResponseEntity.ok(averageAnswers);
+    }
+    @GetMapping("/MaxAnswers")
+    public ResponseEntity<Integer> getMaxAnswers() throws IOException {
+        Integer maxAnswers = jsonNode.get("max_answers").asInt();
+        return ResponseEntity.ok(maxAnswers);
+    }
+    @GetMapping("/AnswerDistribution")
+    public ResponseEntity<JsonNode> getAnswerDistribution() throws IOException {
+        JsonNode answerDistributionNode = jsonNode.get("answer_distribution");
+        return ResponseEntity.ok(answerDistributionNode);
+    }
+
+
+
 
 }
